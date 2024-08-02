a8c864e5570a2323a61171094cf0e09409a2013d
zmxj <935834551@qq.com>
2024-08-02 10:53:10 +0800

    chore: Add class.cpp for class test construct&destruct

--- README.md
+++ README.md
@@ -4,18 +4,21 @@
 - Eigen basic func
 - GN method to solve NLS(naive&ceres&g2o)
 - BA(naive)
+- basic(class)
+- 
 
 ### Todo
+- sort algorithm
 - solve BA(ceres&g2o&GTSAM)
 - basic stl(ptr vector unordered_map)
-- RANSAC(naive&pcl)
+- RANSAC(naive)
 - Triangulation
 - X points methods for F/E/H
 - PnP
 - undistort image(naive&opencv)
 - feature extractor&matcher
 - KD tree build
-- Matrix multiply(naive)
+- Matrix multiply(naive&sparse)
 - line fitting(naive&Hough)
 - Construct OctTree to normalize point cloud
 - Kalman Filter
--- src/basic_stl/class.cpp
+++ src/basic_stl/class.cpp
@@ -9,7 +9,8 @@ public:
     // explicit 关键字，防止隐式类型转换和复制初始化
     // Box b1(1, 2); // OK
     // Box b2 = 1; // Error
-    explicit Box(const Eigen::Vector2f &center, const Eigen::Vector2f &size) : center_(center), size_(size) {
+    explicit Box(const Eigen::Vector2f &center, const Eigen::Vector2f &size) : center_(center), size_(size) 
+    {
         std::cout << "Box constructor" << std::endl;
     }
 
@@ -18,9 +19,18 @@ public:
     // noexcept 修饰成员函数，表示该函数不会抛出异常
     virtual float area() const noexcept
     {
+        std::cout << "Calculate Box area" << std::endl;
         float val;
         val = size_.x() * size_.y();
         return val;
+    }
+
+    
+    // ~Box() 
+    // -> 使用基类指针释放派生类对象时，如果基类的析构函数不是虚函数，那么只会调用基类的析构函数，而不会调用派生类的析构函数
+    virtual ~Box()
+    {
+        std::cout << "Box destructor" << std::endl;
     }
 
 protected:
@@ -31,9 +41,10 @@ protected:
 class Cube : public Box
 {
 public:
-    explicit Cube(const Eigen::Vector2f &center, const Eigen::Vector2f &size, float height)
-        : Box(center, size), height_(height) {
-            std::cout << "Cube constructor" << std::endl;
+    explicit Cube(const Eigen::Vector2f &center, const Eigen::Vector2f &size, float height, int id)
+        : Box(center, size), height_(height), id_(id) 
+        {
+            std::cout << "Cube constructor, ID: " << id_ << std::endl;
         }
 
     // return 4 points, as ld, lt, rt, rd. (r-right)(t-top)(l-left)(d-down)
@@ -53,6 +64,7 @@ public:
     {
         // 显示调用基类的 area 函数
         // float val = Box::area();
+        std::cout << "Calculate Cube area, ID: " << id_ << std::endl;
         float val = 0.f;
         val += 2 * size_.x() * size_.y();
         val += 2 * size_.x() * height_;
@@ -60,17 +72,26 @@ public:
         return val;
     }
 
+    virtual ~Cube()
+    {
+        std::cout << "Cube destructor, ID: " << id_ << std::endl;
+    }
+
 private:
     float height_{0.f};
+    int id_{0};
 };
 
 
 int main()
 {
+    std::cout << "Class example" << std::endl;
+    std::cout << "----------------" << std::endl;
+    std::cout << "Construct " << std::endl;
     Eigen::Vector2f center(0.f, 0.f);
     Eigen::Vector2f size(1.f, 1.f);
     float height = 1.f;
-    Cube cube(center, size, height);
+    Cube cube(center, size, height, 0);
     float cubeArea = cube.area();
     std::cout << "Cube area: " << cubeArea << std::endl;
     auto corners = cube.get_corners();
@@ -78,6 +99,16 @@ int main()
     {
         std::cout << "Cube corners: " << corner.transpose() << std::endl;
     }
+
+    std::cout << "----------------" << std::endl;
+    std::cout << "Destruct " << std::endl;
+    Box* box = new Cube(center, size, height, 1);
+    // malloc 分配内存不能初始化对象，返回void*后进行类型转换
+    // Box* box1 = (Box*)malloc(sizeof(Cube));
+    float boxArea = box->area();
+    std::cout << "Box area: " << boxArea << std::endl;
+    delete box;
+
     return 0;
 }
 
